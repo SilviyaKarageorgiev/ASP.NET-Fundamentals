0b7787c673fcc894b1c41960255fd5bf2e2a82e9
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-05-28 23:01:35 +0300

    Add

---
+++ 04. ASP.NET Core Introduction/CSharpWebFund-MVCIntro-Exercise-May2023/ChatApp/Models/Chat/ChatViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChatApp.Models.Chat
+{
+    public class ChatViewModel
+    {
+        public MessageViewModel CurrentMessage { get; set; } = null!;
+        public ICollection<MessageViewModel> AllMessages { get; set; } = null!;
+    }
+}
