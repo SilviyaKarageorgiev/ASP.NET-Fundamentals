10c527e3a6cb327a4c748034c651c138c8bcfdf3
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-06-02 23:27:08 +0300

    Add

---
+++ 04. ASP.NET Core Introduction/CSharpWebFund-MVCIntro-Exercise-May2023/TextSplitter/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TextSplitter.ViewModels;
+
+namespace TextSplitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(TextSplitViewModel textViewModel)
+        {
+            return View(textViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Split(TextSplitViewModel textViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.RedirectToAction("Index", textViewModel);
+            }
+
+            string[] words = textViewModel.TextToSplit
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string splitText = string.Join(Environment.NewLine, words);
+            textViewModel.SplitText = splitText;
+            return this.RedirectToAction("Index", textViewModel);
+        }
+    }
+}
---
+++ 04. ASP.NET Core Introduction/CSharpWebFund-MVCIntro-Exercise-May2023/TextSplitter/ViewModels/TextSplitViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TextSplitter.ViewModels
+{
+    public class TextSplitViewModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Text to split must be between 2 and 30 characters long!")]
+        public string TextToSplit { get; set; } = null!;
+        public string? SplitText { get; set; }
+    }
+}
