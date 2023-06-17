b4c0da82e73164d2a95c1a91a7ff62c4a0a2664a
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-06-17 13:41:46 +0300

    Add

---
+++ Exam Preparation/Library/Contracts/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using Library.Models;
+
+namespace Library.Contracts
+{
+    public interface IBookService
+    {
+        Task AddBookToCollectionAsync(string userId, BookViewModel book);
+        Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync();
+        Task<BookViewModel?> GetBookByIdAsync(int id);
+        Task<IEnumerable<MineBooksViewModel>> GetMineBooksAsync(string userId);
+    }
+}
---
+++ Exam Preparation/Library/Services/BookService.cs
@@ -0,0 +1,68 @@
+﻿using Library.Contracts;
+using Library.Data;
+using Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly LibraryDbContext dbContext;
+
+        public BookService(LibraryDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public Task AddBookToCollectionAsync(string userId, BookViewModel book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync()
+        {
+            return await dbContext
+                .Books
+                .Select(b => new AllBookViewModel
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    ImageUrl = b.ImageUrl,
+                    Rating = b.Rating,
+                    Category = b.Category.Name
+                }).ToListAsync();
+        }
+
+        public async Task<BookViewModel?> GetBookByIdAsync(int id)
+        {
+            return await dbContext.Books
+                .Where(b => b.Id == id)
+                .Select(b => new BookViewModel
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    ImageUrl = b.ImageUrl,
+                    Description = b.Description,
+                    Rating = b.Rating,
+                    CategoryId = b.CategoryId
+                }).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<MineBooksViewModel>> GetMineBooksAsync(string userId)
+        {
+            return await dbContext.IdentityUserBooks
+                .Where(ub => ub.CollectorId == userId)
+                .Select(b => new MineBooksViewModel
+                {
+                    Id = b.Book.Id,
+                    Title = b.Book.Title,
+                    Author = b.Book.Author,
+                    ImageUrl = b.Book.ImageUrl,
+                    Description = b.Book.Description,
+                    Category = b.Book.Category.Name
+                }).ToListAsync();
+        }
+    }
+}
