3b3149e92b366d9897006c3e37d72c97c309fdeb
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2024-02-04 17:55:22 +0200

    Add

---
+++ Exam Preparation2/10.Exam-Preparation-Library-Skeleton/Library/Data/Models/Book.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Policy;
+
+namespace Library.Data.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MaxLength(50)]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        [MaxLength(5000)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+        public virtual List<IdentityUserBook> UsersBooks { get; set; } = new List<IdentityUserBook>();
+    }
+}
+
+//• Has Id – a unique integer, Primary Key
+//• Has Title – a string with min length 10 and max length 50 (required)
+//• Has Author – a string with min length 5 and max length 50 (required)
+//• Has Description – a string with min length 5 and max length 5000 (required)
+//• Has ImageUrl – a string (required)
+//• Has Rating – a decimal with min value 0.00 and max value 10.00 (required)
+//• Has CategoryId – an integer, foreign key (required)
+//• Has Category – a Category (required)
+//• Has UsersBooks – a collection of type IdentityUserBook
